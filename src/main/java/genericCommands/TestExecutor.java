765c5770f339dd59bfebef9e4a55c979dec487ed
jithendra9092@gmail.com <jithendra9092@gmail.com>
2020-03-06 13:26:10 +0530

    added folders

---
+++ src/main/java/genericCommands/TestExecutor.java
@@ -0,0 +1,40 @@
+package genericCommands;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+
+public class TestExecutor {
+	
+	public WebDriver driver = null;
+	public IndependentCommands generic = null;
+	
+	@BeforeTest
+	public void initializeBrw() throws Exception {
+		generic = new IndependentCommands();
+		generic.intializeBrowser();
+		driver=generic.getDriver();
+		generic.navigateToUrl("https://sel5.fgvms.com/");
+	}
+	
+	@Test
+	public void loginToPage() throws Exception {
+		generic.login(driver);
+		System.out.println("Login Succesfully");
+		//Assert.assertEquals("Fieldglass: System Admin", generic.getTitles());
+		//  return generic.getTitles();
+	}
+	
+	
+	@Test(dependsOnMethods={"loginToPage"})
+	public void main() throws Exception {
+		try {
+			System.out.println(" -----    I am in main method -------  ");
+		}catch(Exception e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
+	}
+}
